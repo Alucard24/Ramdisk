03c7a5e3f323cfb2114124c219fbe8bb31282095
alucard24 <dmbaoh2@gmail.com>
2013-09-16 16:26:01 +0200

    Some adjustments!

--- sbin/ext/post-init.sh
+++ sbin/ext/post-init.sh
@@ -23,19 +23,19 @@ mv  /res/daemonsu /system/xbin/daemonsu
 fi
 
 mv  /res/.has_su_daemon /system/etc/.has_su_daemon
-chmod 0644 /system/etc/.has_su_daemon
+$BB chmod 0644 /system/etc/.has_su_daemon
 
 mv  /res/.installed_su_daemon /system/etc/.installed_su_daemon
-chmod 0644 /system/etc/.installed_su_daemon
+$BB chmod 0644 /system/etc/.installed_su_daemon
 
 mv  /res/install-recovery.sh /system/etc/install-recovery.sh
-chmod 0755 /system/etc/install-recovery.sh
+$BB chmod 0755 /system/etc/install-recovery.sh
 
-chmod -R 6755 /sbin
+$BB chmod -R 6755 /sbin
 chown 0.0 /system/xbin/su
-chmod 06755 /system/xbin/su
+$BB chmod 6755 /system/xbin/su
 chown 0.0 /system/xbin/daemonsu
-chmod 06755 /system/xbin/daemonsu
+$BB chmod 6755 /system/xbin/daemonsu
 symlink /system/xbin/su /system/bin/su
 
 if [ ! -f /system/app/Superuser.apk ]; then
@@ -43,7 +43,7 @@ mv /res/Superuser.apk /system/app/Superuser.apk
 fi
 
 chown 0.0 /system/app/Superuser.apk
-chmod 0644 /system/app/Superuser.apk
+$BB chmod 0644 /system/app/Superuser.apk
 
 if [ ! -f /system/xbin/busybox ]; then
 ln -s /sbin/busybox /system/xbin/busybox
@@ -58,29 +58,29 @@ fi
 if [ ! -f /system/app/STweaks.apk ]; then
   cat /res/STweaks.apk > /system/app/STweaks.apk
   chown 0.0 /system/app/STweaks.apk
-  chmod 644 /system/app/STweaks.apk
+  $BB chmod 644 /system/app/STweaks.apk
 fi
 
-chmod 755 /res/customconfig/actions/controlswitch
-chmod 755 /res/customconfig/actions/generic
-chmod 755 /res/customconfig/actions/generic01
-chmod 755 /res/customconfig/actions/generictag
-chmod 755 /res/customconfig/actions/iosched
-chmod 755 /res/customconfig/actions/cpugeneric
-chmod 755 /res/customconfig/actions/cpuvolt
-chmod 755 /res/customconfig/customconfig-helper
-chmod 755 /res/customconfig/customconfig.xml.generate
+$BB chmod 755 /res/customconfig/actions/controlswitch
+$BB chmod 755 /res/customconfig/actions/generic
+$BB chmod 755 /res/customconfig/actions/generic01
+$BB chmod 755 /res/customconfig/actions/generictag
+$BB chmod 755 /res/customconfig/actions/iosched
+$BB chmod 755 /res/customconfig/actions/cpugeneric
+$BB chmod 755 /res/customconfig/actions/cpuvolt
+$BB chmod 755 /res/customconfig/customconfig-helper
+$BB chmod 755 /res/customconfig/customconfig.xml.generate
 
 rm /data/.alucard/customconfig.xml
 rm /data/.alucard/action.cache
-chmod -R 6777 /data/.alucard
+$BB chmod -R 6777 /data/.alucard
 
 /system/bin/setprop pm.sleep_mode 1
 /system/bin/setprop ro.ril.disable.power.collapse 0
 /system/bin/setprop ro.telephony.call_ring.delay 1000
 
 mkdir -p /mnt/ntfs
-chmod 777 /mnt/ntfs
+$BB chmod 777 /mnt/ntfs
 mount -o mode=0777,gid=1000 -t tmpfs tmpfs /mnt/ntfs
 
 (
@@ -99,9 +99,15 @@ if [ -d /system/etc/init.d ]; then
 fi
 
 (
-	sleep 20
-	chmod 755 /res/uci.sh
+	#sleep 20
+	$BB mount -o remount,rw rootfs
+	$BB chown -R root:system /res/customconfig/actions/;
+	$BB chmod -R 6755 /res/customconfig/actions/;
+	$BB chmod 6755 /res/uci.sh
 	$BB sh /res/uci.sh apply
-	$BB mount -t rootfs -o remount,ro rootfs
 	mount -o remount,ro /system
+	mount -t rootfs -o remount,ro rootfs
+	#mount -o remount,rw /system;
+	#mount -o remount,rw /;
 )&
+
