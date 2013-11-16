88305461d64602ef701310decb74050ad6ba1dcb
Alucard24 <dmbaoh2@gmail.com>
2013-11-16 17:23:41 +0100

    Fixed some files!

--- init.qcom.syspart_fixup.sh
+++ init.qcom.syspart_fixup.sh
@@ -63,9 +63,9 @@ fi
 setprop ro.modem.links.done 1
 
 # Run thermal script
-#if [ -f /system/etc/init.qcom.thermal_conf.sh ]; then
-#  /system/bin/sh /system/etc/init.qcom.thermal_conf.sh
-#fi
+if [ -f /system/etc/init.qcom.thermal_conf.sh ]; then
+  /system/bin/sh /system/etc/init.qcom.thermal_conf.sh
+fi
 
 # Run wifi script
 if [ -f /system/etc/init.qcom.wifi.sh ]; then
@@ -79,7 +79,5 @@ fi
 
 # This should be the last command
 # remount system as read-only.
-  mount -o ro,remount,barrier=1 /system
-
-
+mount -o ro,remount,barrier=1 /system
 
