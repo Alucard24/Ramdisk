bdb07d6c7a99e383ac3ff39750ed98d5224d723f
Alucard24 <dmbaoh2@gmail.com>
2014-01-13 23:00:28 +0100

    Removed stweaks installation, uninstallation during system startup!

--- sbin/ext/install.sh
+++ sbin/ext/install.sh
@@ -46,41 +46,41 @@ if [ ! -f /system/bin/busybox ]; then
 	$BB ln -s /sbin/busybox /system/bin/pkill;
 fi;
 
-if [ -f /system/app/Extweaks.apk ] || [ -f /data/app/com.darekxan.extweaks.ap*.apk ]; then
-	$BB rm -f /system/app/Extweaks.apk > /dev/null 2>&1;
-	$BB rm -f /data/app/com.darekxan.extweaks.ap*.apk > /dev/null 2>&1;
-	$BB rm -rf /data/data/com.darekxan.extweaks.app > /dev/null 2>&1;
-	$BB rm -f /data/dalvik-cache/*com.darekxan.extweaks.app* > /dev/null 2>&1;
-fi;
+# if [ -f /system/app/Extweaks.apk ] || [ -f /data/app/com.darekxan.extweaks.ap*.apk ]; then
+# 	$BB rm -f /system/app/Extweaks.apk > /dev/null 2>&1;
+# 	$BB rm -f /data/app/com.darekxan.extweaks.ap*.apk > /dev/null 2>&1;
+# 	$BB rm -rf /data/data/com.darekxan.extweaks.app > /dev/null 2>&1;
+# 	$BB rm -f /data/dalvik-cache/*com.darekxan.extweaks.app* > /dev/null 2>&1;
+# fi;
 
-STWEAKS_CHECK=$($BB find /data/app/ -name com.gokhanmoral.stweaks* | wc -l);
+# STWEAKS_CHECK=$($BB find /data/app/ -name com.gokhanmoral.stweaks* | wc -l);
 
-if [ "$STWEAKS_CHECK" -eq "1" ]; then
-	$BB rm -f /data/app/com.gokhanmoral.stweaks* > /dev/null 2>&1;
-	$BB rm -f /data/data/com.gokhanmoral.stweaks*/* > /dev/null 2>&1;
-fi;
+# if [ "$STWEAKS_CHECK" -eq "1" ]; then
+# 	$BB rm -f /data/app/com.gokhanmoral.stweaks* > /dev/null 2>&1;
+# 	$BB rm -f /data/data/com.gokhanmoral.stweaks*/* > /dev/null 2>&1;
+# fi;
 
-if [ -f /system/app/STweaks.apk ]; then
-	stmd5sum=$($BB md5sum /system/app/STweaks.apk | $BB awk '{print $1}');
-	stmd5sum_kernel=$(cat /res/stweaks_md5);
-	if [ "$stmd5sum" != "$stmd5sum_kernel" ]; then
-		$BB rm -f /system/app/STweaks.apk > /dev/null 2>&1;
-		$BB rm -f /data/data/com.gokhanmoral.stweaks*/* > /dev/null 2>&1;
-		$BB rm -f /data/dalvik-cache/*gokhanmoral.*weak*.apk* > /dev/null 2>&1;
-		$BB rm -f /cache/dalvik-cache/*gokhanmoral.*weak*.apk* > /dev/null 2>&1;
-		$BB cp /res/STweaks.apk /system/app/;
-		$BB chown 0.0 /system/app/STweaks.apk;
-		$BB chmod 644 /system/app/STweaks.apk;
-	fi;
-else
-	$BB rm -f /data/app/com.gokhanmoral.*weak*.apk > /dev/null 2>&1;
-	$BB rm -r /data/data/com.gokhanmoral.*weak*/* > /dev/null 2>&1;
-	$BB rm -f /data/dalvik-cache/*gokhanmoral.*weak*.apk* > /dev/null 2>&1;
-	$BB rm -f /cache/dalvik-cache/*gokhanmoral.*weak*.apk* > /dev/null 2>&1;
-	$BB cp -a /res/STweaks.apk /system/app/;
-	$BB chown 0.0 /system/app/STweaks.apk;
-	$BB chmod 644 /system/app/STweaks.apk;
-fi;
+# if [ -f /system/app/STweaks.apk ]; then
+# 	stmd5sum=$($BB md5sum /system/app/STweaks.apk | $BB awk '{print $1}');
+# 	stmd5sum_kernel=$(cat /res/stweaks_md5);
+# 	if [ "$stmd5sum" != "$stmd5sum_kernel" ]; then
+# 		$BB rm -f /system/app/STweaks.apk > /dev/null 2>&1;
+# 		$BB rm -f /data/data/com.gokhanmoral.stweaks*/* > /dev/null 2>&1;
+# 		$BB rm -f /data/dalvik-cache/*gokhanmoral.*weak*.apk* > /dev/null 2>&1;
+# 		$BB rm -f /cache/dalvik-cache/*gokhanmoral.*weak*.apk* > /dev/null 2>&1;
+# 		$BB cp /res/STweaks.apk /system/app/;
+# 		$BB chown 0.0 /system/app/STweaks.apk;
+# 		$BB chmod 644 /system/app/STweaks.apk;
+# 	fi;
+# else
+# 	$BB rm -f /data/app/com.gokhanmoral.*weak*.apk > /dev/null 2>&1;
+# 	$BB rm -r /data/data/com.gokhanmoral.*weak*/* > /dev/null 2>&1;
+# 	$BB rm -f /data/dalvik-cache/*gokhanmoral.*weak*.apk* > /dev/null 2>&1;
+# 	$BB rm -f /cache/dalvik-cache/*gokhanmoral.*weak*.apk* > /dev/null 2>&1;
+# 	$BB cp -a /res/STweaks.apk /system/app/;
+# 	$BB chown 0.0 /system/app/STweaks.apk;
+# 	$BB chmod 644 /system/app/STweaks.apk;
+# fi;
 
 $BB mount -o remount,rw /;
 $BB mount -o remount,rw /system;
