cdc1f44bf429502b34bf16e48eee6eecfa4fdca4
Alucard24 <dmbaoh2@gmail.com>
2014-04-26 22:21:09 +0200

    F2FS or EXT4 filesystem, supported. With this new modifications we can do a lot of things.

---
+++ check_fs.sh
@@ -0,0 +1,48 @@
+#!/sbin/busybox sh
+# Copyright (c) 2012, Code Aurora Forum. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#     * Neither the name of Code Aurora Forum, Inc. nor the names of its
+#       contributors may be used to endorse or promote products derived
+#      from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+BB=/sbin/busybox
+
+$BB mount -o remount,rw /system;
+$BB mount -o remount,rw /;
+
+# mount existing cache partition on /tmp/fscache
+$BB mkdir /tmp/fscache;
+$BB mount -t f2fs /dev/block/platform/msm_sdcc.1/by-name/cache /tmp/fscache;
+
+if [ `$BB mount | grep "/fscache" | wc -l` -eq "1" ]; then
+	$BB umount /tmp/fscache;
+	$BB mv /fstab.qcom /fstab.old;
+	$BB mv /fstab.f2fs /fstab.qcom;
+else
+	$BB mv /fstab.qcom /fstab.old;
+	$BB mv /fstab.ext4 /fstab.qcom;
+fi;
+
+$BB rm -r /tmp/fscache;
