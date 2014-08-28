87ab25acd9a3f7f5443d90d69561a8dbd7d98eef
alucard24 <dmbaoh2@gmail.com>
2014-08-28 14:44:26 +0200

    updated ramdisk to that i have lost :(
    Conflicts:
    	sbin/cksum

--- sbin/ext/busybox.sh
+++ sbin/ext/busybox.sh
@@ -8,12 +8,12 @@ $BB mount -o remount,rw /;
 # installe latest busybox to ROM
 $BB cp /sbin/busybox /system/xbin/;
 /system/xbin/busybox --install -s /system/xbin/
-#if [ -e /system/xbin/wget ]; then
-#	rm /system/xbin/wget;
-#fi;
-#if [ -e /system/wget/wget ]; then
-#	chmod 755 /system/wget/wget;
-#fi;
+if [ -e /system/xbin/wget ]; then
+	rm /system/xbin/wget;
+fi;
+if [ -e /system/wget/wget ]; then
+	chmod 755 /system/wget/wget;
+fi;
 chmod 06755 /system/xbin/busybox;
 chmod 06755 /system/xbin/su;
 chmod 06755 /system/xbin/daemonsu;
