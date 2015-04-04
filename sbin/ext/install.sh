5f15cf5249eda84815facac75877dc1d7a55c976
alucard24 <dmbaoh2@gmail.com>
2015-04-04 13:50:35 +0200

    updated ramdisk from @dorimanx branch

--- sbin/ext/install.sh
+++ sbin/ext/install.sh
@@ -2,15 +2,6 @@
 
 BB=/sbin/busybox
 
-ROOTFS_MOUNT=$(mount | grep rootfs | cut -c26-27 | grep rw | wc -l)
-SYSTEM_MOUNT=$(mount | grep system | cut -c69-70 | grep rw | wc -l)
-if [ "$ROOTFS_MOUNT" -eq "0" ]; then
-	$BB mount -o remount,rw /;
-fi;
-if [ "$SYSTEM_MOUNT" -eq "0" ]; then
-	$BB mount -o remount,rw /system;
-fi;
-
 cd /;
 
 fi;
