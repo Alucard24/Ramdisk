5f065324d5660cdb02860083440d45256a3b47a2
alucard24 <dmbaoh2@gmail.com>
2015-08-16 14:38:26 +0200

    BOOT SCRIPT: disable debug for kernel drivers. Thanks to @dorimanx

--- sbin/ext/post-init.sh
+++ sbin/ext/post-init.sh
@@ -212,16 +212,25 @@ MODULES_LOAD()
 # disable debugging on some modules
 if [ "$android_logger" -ge "1" ]; then
 	echo "N" > /sys/module/kernel/parameters/initcall_debug;
-#	echo "0" > /sys/module/alarm/parameters/debug_mask;
-#	echo "0" > /sys/module/alarm_dev/parameters/debug_mask;
-#	echo "0" > /sys/module/binder/parameters/debug_mask;
-	echo "0" > /sys/module/xt_qtaguid/parameters/debug_mask;
-#	echo "0" > /sys/kernel/debug/clk/debug_suspend;
-#	echo "0" > /sys/kernel/debug/msm_vidc/debug_level;
-#	echo "0" > /sys/module/ipc_router/parameters/debug_mask;
-#	echo "0" > /sys/module/msm_serial_hs/parameters/debug_mask;
-#	echo "0" > /sys/module/msm_show_resume_irq/parameters/debug_mask;
-#	echo "0" > /sys/module/pm_8x60/parameters/debug_mask;
+	echo "0" > /sys/module/smd/parameters/debug_mask
+	echo "0" > /sys/module/rpm_regulator_smd/parameters/debug_mask
+	echo "0" > /sys/module/ipc_router/parameters/debug_mask
+	echo "0" > /sys/module/event_timer/parameters/debug_mask
+	echo "0" > /sys/module/msm_serial_hs/parameters/debug_mask
+#	echo "0" > /sys/module/powersuspend/parameters/debug_mask
+#	echo "0" > /sys/module/msm_hotplug/parameters/debug_mask
+#	echo "0" > /sys/module/cpufreq_limit/parameters/debug_mask
+#	echo "0" > /sys/module/rpm_smd/parameters/debug_mask
+	echo "0" > /sys/module/smd_pkt/parameters/debug_mask
+	echo "0" > /sys/module/xt_qtaguid/parameters/debug_mask
+	echo "0" > /sys/module/binder/parameters/debug_mask
+	echo "0" > /sys/module/msm_show_resume_irq/parameters/debug_mask
+	echo "0" > /sys/module/alarm_dev/parameters/debug_mask
+	echo "0" > /sys/module/pm_8x60/parameters/debug_mask;
+	echo "0" > /sys/module/spm_v2/parameters/debug_mask
+	echo "0" > /sys/module/alu_t_boost/parameters/debug_mask
+	echo "0" > /sys/module/ipc_router_smd_xprt/parameters/debug_mask
+	echo "0" > /sys/module/x_tables/parameters/debug_mask
 fi;
 
 OPEN_RW;
